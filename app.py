c3f02667f76f7122a6f15417c4c749dc0f95c436
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-13 22:02:03 +0100

    try to fix systemd service bug

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ def header_title():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # use_reloader=False should fix the systemd
+    # service quitting (don't ask me why...)
+    app.run(port=5000, debug=True, use_reloader=False)
