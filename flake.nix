b429ace6cf9ab4dcedb360d439a4c3cb64a5cd1a
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-14 17:09:36 +0100

    try using the -wrapped script instead

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
               };
 
               systemd.tmpfiles.rules = [ "d '/var/lib/filmvisarna-backend' 0750 ejg users -" 
-                                         "L+ '/var/lib/filmvisarna-backend/filmvisarna-backend.py' - - - - ${pkg}/bin/filmvisarna-backend.py"];
+                                         "L+ '/var/lib/filmvisarna-backend/filmvisarna-backend.py' - - - - ${pkg}/bin/.filmvisarna-backend.py-wrapped"];
 
               systemd.services."${name}-flask" = let
                 gunicorn = pkgs.python3Packages.gunicorn;
