deab5b810e0e5cbc4db5663831d37871f855ad7e
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-17 03:42:14 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='filmvisarna-backend',
-      version='1.0',
+      version='1.1',
       # Modules to import from other scripts:
       packages=find_packages(),
       # Executables
