6bf7d60153c47e2c0fb901fe7c7c388df0c775c9
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-19 20:01:46 +0100

    add the 3 word operators

--- endpoint_maker.py
+++ endpoint_maker.py
@@ -4,6 +4,12 @@ from flask import jsonify, request
 from collections import defaultdict 
 from pprint import pprint
 
+
+# OFFSET can't exist without some LIMIT
+# this is the default LIMIT for such cases.
+REALLY_LARGE_NUMBER = 999999
+
+
 class KeyDefaultDict(defaultdict):
     def __missing__(self, key):
         if self.default_factory is None:
@@ -17,6 +23,10 @@ OPERATORS = ('>', '<', '!~', '~', '!')
 OPERATOR_TO_WHERE = KeyDefaultDict(lambda key: f'{{k}} {key} {{v}}')  
 OPERATOR_TO_WHERE['~'] = "{k} REGEXP '{v}'"
 OPERATOR_TO_WHERE['!~'] = "{k} NOT REGEXP '{v}'"
+WORD_OPS = ('sort', 'limit', 'offset')
+WORD_OPS_LOOKUP_DICT = {k: i for i, k in enumerate(WORD_OPS)}
+def word_ops_tuple_sort(tup):
+    return WORD_OPS_LOOKUP_DICT.get(tup[0])
 
 def get_search_ops(request):
     search_ops = [] 
@@ -30,18 +40,35 @@ def get_search_ops(request):
                 val = args[1] if args[1] else v 
                 search_ops.append((key, op, val))
                 break
-        else:  # assume =
+        else:
             search_ops.append((k, '=', v))
     return search_ops
 
 def col_filters(search_ops):
     if not search_ops: return ''
-    s = ' WHERE '
     filters = [OPERATOR_TO_WHERE[op].format(k=k, v=v)
-               for (k, op, v) in search_ops]
-    return s + ' AND '.join(filters)
+               for (k, op, v) in search_ops
+               if k not in WORD_OPS]
+    s = ' WHERE ' + ' AND '.join(filters) if filters else ''
 
+    # add on word operators
+    has_limit = any(k == 'limit' for (k, op, v) in search_ops) 
+    has_offset = any(k == 'offset' for (k, op, v) in search_ops)
+    use_big_limit = has_offset and not has_limit
+    for (k, op, v) in sorted(search_ops, key=word_ops_tuple_sort):
+        if k == 'sort':
+            direction = 'ASC'  # default
+            if v[-1] == '-':
+                direction = 'DESC'
+                v = v[:-1]  # cut off -
+            s += f' ORDER BY {v} {direction}'
+        if k == 'limit': 
+            v = REALLY_BIG_NUMBER if use_big_limit else v
+            s += f' LIMIT {v}' 
+        if k == 'offset': 
+            s += f' OFFSET {v}'
 
+    return s
 
 def request_params(*args):
     return tuple(request.json[arg] for arg in args)
